9687a592f1ba2d90a0d099cda853d4e5967882cb
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2024-11-20 21:56:32 -0300

    Corrected the implementations of the example app according to the use of the option of ZERO and EPOCH for choosing between different time-stamp format.

--- buggy app self-logging/main.c
+++ buggy app self-logging/main.c
@@ -16,7 +16,7 @@ extern stopwatch reporting_clk;
 
 int main( void ){
     // [ INSTRUMENTACION: Inicialización del timer. ]
-    start (&reporting_clk);
+    start (&reporting_clk, ZERO);
     //
     // [ INSTRUMENTACION: Agregado para poder enviar el string especificando el evento. ]
     char str[MAX_EVENT_SIZE];
